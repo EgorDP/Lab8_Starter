f37251e428224ab1638b28d39e6326612f8cad03
EgorDP <egorpustovalo8@gmail.com>
2021-11-14 21:34:47 -0800

    Finish expose section

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Lab 8 - Starter
+1. I would select option 1 so that the tests run whenever I push to Github. This makes the most sense as it allows me to not worry about testing like option 2 does, but also means that I get tests during development unlike 3. 
+2. No you would use unit tests since those are meant to capture the output of function while end-to-end testing is meant for user actions.
+3. No I would not use a unit test for this. For sending a message I would need to have access to the text area and the button that the user
+would type in and then send the message with, respectively. This would involve emulating a user action and this is where end-to-end testing
+using puppeteer would come into play. 
+4. It is possible that a unit test could help. This function could potentially track the length of the text area box and output false to some sort of keepTyping boolean variable when the character limit is reached. A unit test could assert that this is the case. <br>
+However, a UI test would probably be more beneficial since the program could write in the text area and then see if writing is possible once that 80 character limit is reached. 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -1,5 +1,76 @@
 // unit.test.js
 
+const { exportAllDeclaration } = require('@babel/types');
+const { TestWatcher } = require('@jest/core');
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
-// TODO - Part 2
+// Phone Numbers
+test('Checks if phone number is a valid phone number #1', () => {
+    expect(functions.isPhoneNumber('(623)878-3123')).toBe(true);
+});
+test('Checks if phone number is a valid phone number #2', () => {
+    expect(functions.isPhoneNumber('(000)420-3790')).toBe(true);
+});
+test('Checks invalid phone number #1', () => {
+    expect(functions.isPhoneNumber('aef')).toBe(true);
+});
+test('Checks invalid phone number #2', () => {
+    expect(functions.isPhoneNumber('33372819129129')).toBe(true);
+});
+
+// Valid email check
+test('Checks valid email #1', () => {
+    expect(functions.isEmail('joseph@gmail.com')).toBe(true);
+});
+test('Checks valid email #2', () => {
+    expect(functions.isEmail('hobber@ucsd.edu')).toBe(true);
+});
+test('Checks invalid email #1', () => {
+    expect(functions.isEmail('hobboaefu')).toBe(true);
+});
+test('Checks invalid email #2', () => {
+    expect(functions.isEmail('johnny rules')).toBe(true);
+});
+
+// Strong password check 
+test('Checks valid strong password #1', () => {
+    expect(functions.isStrongPassword('A1209E')).toBe(true);
+});
+test('Checks valid strong password #2', () => {
+    expect(functions.isStrongPassword('o8273230_')).toBe(true);
+});
+test('Checks invalid strong password #1', () => {
+    expect(functions.isStrongPassword('8a98afe98')).toBe(true);
+});
+test('Checks invalid strong password #2', () => {
+    expect(functions.isStrongPassword('yo')).toBe(true);
+});
+
+// Date check
+test('Checks valid date #1', () => {
+    expect(functions.isDate('01/32/2001')).toBe(true);
+});
+test('Checks valid date #2', () => {
+    expect(functions.isDate('23/32/2332')).toBe(true);
+});
+test('Checks invalid date #1', () => {
+    expect(functions.isDate('urDate')).toBe(true);
+});
+test('Checks invalid date #2', () => {
+    expect(functions.isDate('230912/230/23923')).toBe(true);
+});
+
+// Hex color
+
+test('Checks valid hex color #1', () => {
+    expect(functions.isHexColor('#AC5B4A')).toBe(true);
+});
+test('Checks valid hex color #2', () => {
+    expect(functions.isHexColor('#F8451F')).toBe(true);
+});
+test('Checks invalid hex color #1', () => {
+    expect(functions.isHexColor('a fine hex color ')).toBe(true);
+});
+test('Checks invalid hex color #2', () => {
+    expect(functions.isHexColor('28si29')).toBe(true);
+});
